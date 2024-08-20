06010f0a4cb4abb79eb61a5c01b12c53d7d8d394
Error-String-Expected-Got-Nil <103805191+Error-String-Expected-Got-Nil@users.noreply.github.com>
2024-08-20 16:51:54 -0500

    Head location functions
    Also a ThingComp to test it

---
+++ Source/RainRim/Debug/ThingComp_RenderOnHead.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using RainRim.Utils;
+using Verse;
+
+namespace RainRim.Debug;
+
+// ReSharper disable InconsistentNaming
+
+// Debug comp to test that the TransformVectorByPawn and GetBaseHeadOffset functions worked correctly
+public class ThingComp_RenderOnHead : ThingComp
+{
+    public Graphic RenderGraphic;
+
+    public override void Initialize(CompProperties properties)
+    {
+        props = properties;
+
+        RenderGraphic = ((CompProperties_RenderOnHead)props).graphicData.Graphic;
+    }
+
+    public override void PostDraw()
+    {
+        if (parent is not Pawn pawn || parent.DrawPosHeld is not { } drawPos) return;
+        var headOffset = MathUtils.TransformVectorByPawn(MathUtils.GetBaseHeadOffset(pawn), pawn);
+        
+        RenderGraphic.DrawWorker((drawPos + headOffset).WithY(pawn.def.Altitude + Altitudes.AltInc), Rot4.North,
+            null, null, 0f);
+    }
+}
+
+public class CompProperties_RenderOnHead : CompProperties
+{
+    public GraphicData graphicData;
+
+    public CompProperties_RenderOnHead()
+    {
+        compClass = typeof(ThingComp_RenderOnHead);
+    }
+    
+    public override IEnumerable<string> ConfigErrors(ThingDef parentDef)
+    {
+        foreach(var error in base.ConfigErrors(parentDef)) yield return error;
+
+        if (graphicData == null) yield return parentDef.defName + " has RenderOnHead comp with no GraphicData";
+    }
+}
--- Source/RainRim/Utils/MathUtils.cs
+++ Source/RainRim/Utils/MathUtils.cs
@@ -1,10 +1,21 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using RimWorld;
+using UnityEngine;
 using Verse;
 
 namespace RainRim.Utils;
 
 public static class MathUtils
 {
+    // Default head offsets for animals if their RaceProps don't define them explicitly
+    private static readonly List<Vector3> GenericAnimalHeadOffsets =
+    [
+        new Vector3(0f, 0f, 0.4f),
+        new Vector3(0.4f, 0f, 0.25f),
+        new Vector3(0f, 0f, 0.1f),
+        new Vector3(-0.4f, 0f, 0.25f)
+    ];
+    
     // Takes a vector, returning it scaled and rotated based on a pawn's graphic data and body. If the initial vector
     // represents an offset from the center of the pawn's base sprite, the returned vector will be the same point on
     // the pawn's drawn sprite.
@@ -15,4 +26,12 @@ public static class MathUtils
         return new Vector3(vec.x * scaling.x, vec.y, vec.z * scaling.y)
             .RotatedBy(pawn.Drawer.renderer.BodyAngle(PawnRenderFlags.None));
     }
+
+    // Gets the base offset relative to the center of a pawn's sprite that represents the location of their head.
+    public static Vector3 GetBaseHeadOffset(Pawn pawn) 
+        => pawn.RaceProps.Humanlike 
+            ? pawn.Drawer.renderer.BaseHeadOffsetAt(pawn.Rotation) 
+            : pawn.RaceProps.headPosPerRotation.NullOrEmpty() 
+                ? GenericAnimalHeadOffsets[pawn.Rotation.AsInt] 
+                : pawn.RaceProps.headPosPerRotation[pawn.Rotation.AsInt];
 }
