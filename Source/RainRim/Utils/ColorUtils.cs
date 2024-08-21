d7e736e89f0cb9ecbd3c298b2de8517d6d6272e7
Error-String-Expected-Got-Nil <103805191+Error-String-Expected-Got-Nil@users.noreply.github.com>
2024-08-21 17:01:25 -0500

    Layered graphics initial
    Quite incomplete. Currently looking into how PawnRenderNodes work so I can use those, since they should be better than trying to draw the graphics with the comp directly.

---
+++ Source/RainRim/Utils/ColorUtils.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace RainRim.Utils;
+
+// ReSharper disable InconsistentNaming
+
+public static class ColorUtils
+{
+    // Assumes inputs are all on range [0, 1]
+    public static Color HSL2RGB(float hue, float saturation, float lightness)
+    {
+        var chroma = (1f - Mathf.Abs(2f * lightness - 1f)) * saturation;
+
+        if (chroma == 0f) return new Color(lightness, lightness, lightness);
+        
+        var secondary = chroma * (1f - Mathf.Abs(hue * 6f % 2f - 1f));
+        var baseline = lightness - chroma * 0.5f;
+
+        float red = 0f, green = 0f, blue = 0f;
+        switch ((int)(hue * 6f) % 6)
+        {
+            case 0:
+                red = chroma;
+                green = secondary;
+                break;
+            case 1:
+                red = secondary;
+                green = chroma;
+                break;
+            case 2:
+                green = chroma;
+                blue = secondary;
+                break;
+            case 3:
+                green = secondary;
+                blue = chroma;
+                break;
+            case 4:
+                red = secondary;
+                blue = chroma;
+                break;
+            case 5:
+                red = chroma;
+                blue = secondary;
+                break;
+        }
+
+        return new Color(red + baseline, green + baseline, blue + baseline);
+    }
+}
---
+++ Source/RainRim/Utils/GradientPalette.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace RainRim.Utils;
+
+// ReSharper disable InconsistentNaming
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable FieldCanBeMadeReadOnly.Global
+// ReSharper disable ConvertToConstant.Global
+
+public class GradientPalette
+{
+    // Base is the baseline value. Variation is the maximum deviation from that value, either positively or negatively.
+    // Curve is the value of k for the WeightedCurve function.
+    public float hueBase = 0f;
+    public float hueVariation = 0f;
+    public float hueCurve = 1f;
+    
+    public float satBase = 1f;
+    public float satVariation = 0f;
+    public float satCurve = 1f;
+    
+    public float litBase = 0.5f;
+    public float litVariation = 0f;
+    public float litCurve = 1f;
+
+    // Hue is wrapped around to fit into range [0, 1), sat and lit are clamped to [0, 1]
+    public Color PickColor(float hueFactor, float satFactor, float litFactor) 
+        => ColorUtils.HSL2RGB(
+            (hueBase + hueVariation * WeightedCurve(hueFactor, hueCurve)) % 1, 
+            Mathf.Clamp01(satBase + satVariation * WeightedCurve(satFactor, satCurve)), 
+            Mathf.Clamp01(litBase + litVariation * WeightedCurve(litFactor, litCurve)));
+
+    // Maps domain [0, 1] to range [-1, 1] on a weighted curve. This is the simplified form of a set of functions
+    // that Rain World actually uses when randomly selecting colors for things. Not using the original system since
+    // that'd be excessive (and it feels wrong to just verbatim copy decompiled code).
+    //
+    // The k value is on range (0, inf). The shape of the curve asymptotically approaches a linear function as k 
+    // increases, and is more heavily weighted towards the ends at lower values of k. Don't use k = 0, it is a flat
+    // line at y = 0.
+    public static float WeightedCurve(float x, float k)
+    {
+        x = x * 2f - 1f;
+        return k * Mathf.Abs(x) / (k - Mathf.Abs(x) + 1f) * Mathf.Sign(x);
+    }
+}
