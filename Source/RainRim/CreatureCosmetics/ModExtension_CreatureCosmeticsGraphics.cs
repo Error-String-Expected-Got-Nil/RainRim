74e89f8f5e9fb41217c6d2a60c36f1d8530160dd
Error-String-Expected-Got-Nil <103805191+Error-String-Expected-Got-Nil@users.noreply.github.com>
2024-08-22 12:58:01 -0500

    Lizard rendering start
    So I can work on it elsewhere

---
+++ Source/RainRim/CreatureCosmetics/ModExtension_CreatureCosmeticsGraphics.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace RainRim.CreatureCosmetics;
+
+// ReSharper disable InconsistentNaming
+
+public class ModExtension_CreatureCosmeticGraphics : DefModExtension
+{
+    public Dictionary<string, GraphicData> cosmeticGraphics;
+}
---
+++ Source/RainRim/CreatureCosmetics/PawnRenderNode_CreatureCosmetics.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Verse;
+
+namespace RainRim.CreatureCosmetics;
+
+// ReSharper disable InconsistentNaming
+
+public class PawnRenderNode_CreatureCosmetics : PawnRenderNode
+{
+    public Graphic CosmeticGraphic;
+    
+    public PawnRenderNode_CreatureCosmetics(Pawn pawn, PawnRenderNodeProperties props, PawnRenderTree tree)
+        : base(pawn, props, tree)
+    {
+        if (pawn.kindDef.GetModExtension<ModExtension_CreatureCosmeticGraphics>() is { } cosmetics)
+        {
+            
+        }
+    }
+    
+    
+}
+
+public class PawnRenderNodeProperties_CreatureCosmetics : PawnRenderNodeProperties
+{
+    public string graphicKey;
+    
+    public PawnRenderNodeProperties_CreatureCosmetics()
+    {
+        nodeClass = typeof(PawnRenderNode_CreatureCosmetics);
+    }
+}
